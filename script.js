a176966b32a9f7b7179ade83ab19809f3f8cb0e1
Abdul Qadir Imran <abdulqadirimran01@gmail.com>
2024-05-11 08:38:46 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const form = document.getElementById('form')
 const input = document.getElementById('input')
+
 const todosUL = document.getElementById('todos')
 
 const todos = JSON.parse(localStorage.getItem('todos'))
