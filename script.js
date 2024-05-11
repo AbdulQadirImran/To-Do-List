d068c7f1c7fcb75232e7ae4c10e2be6e8a59d91c
Abdul Qadir Imran <abdulqadirimran01@gmail.com>
2024-05-11 08:39:05 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const input = document.getElementById('input')
 const todosUL = document.getElementById('todos')
 
 const todos = JSON.parse(localStorage.getItem('todos'))
-
 if(todos) {
     todos.forEach(todo => addTodo(todo))
 }
